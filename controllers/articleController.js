8e7090ec2a49573f3e409fa43756844c93f922e8
Daniikpando <daniikpando13@hotmail.com>
2018-05-11 00:23:08 -0500

    Se realizaron los procesos de update, create y delete de la coleccion article

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -13,7 +13,16 @@ class ArticleController {
             if (docs.length == 0) return _response(res, 404, "No se encontro ningún registro en la base de datos");
             _response(res, 200, "La solicitud fue procesada exitosamente", docs);
         }).limit(limit)
-          .skip(skip);
+          .skip(skip)
+          .sort({ id: 'asc'});
+    }
+    
+    static _execArticles(obj, res) {
+        console.log(obj.title)
+        obj.save( (err, doc) => {
+            if(err) return _response(res, 500, "No se pudo realizar la accion solicitada", err);
+            _response(res, 201, "Se realizo la accion solicitada exitosamente",doc);
+        });
     }
 
     index(req, res) {
@@ -40,17 +49,46 @@ class ArticleController {
     } 
     show(req, res) {
         let id = req.params.id;
-        console.log(id)
         ArticleController._findArticles(res, {id: id}, 0, 1);
     }
     create(req, res) {
+        let article = new Article();
 
+        article.id = req.body.id;
+        article.title = req.body.title;
+        article.content = req.body.content;
+        article.description = req.body.description;
+
+        ArticleController._execArticles(article, res);
     }
+
     update(req, res) {
+        let id = parseInt(req.params.id);
+
+        Article.findOne({id: id}, (err, article) => {
+            
+            if(err) return _response(res, 500, "No se pudo actualizar el registro article", err);
+            if(article == null) return _response(res, 404, "No se encontro el recurso que solicitaba")
+            
+            article.title = req.body.title;
+            article.content = req.body.content;
+            article.description = req.body.description;
+
+            ArticleController._execArticles(article, res);
+        });
 
     }
     delete(req, res) {
-        
+        let id = parseInt(req.params.id);
+
+        Article.deleteOne({id: id}, (err, result) => {
+            if(err) return _response(res, 500, "No se pudo eliminar el recurso solicitado", err)
+            
+            let filas = result.n;
+            if(filas == 0) return _response(res, 404, "No se encontro el recurso que se queria elminiar")
+
+            _response(res, 202, "Se elimino el recurso exitosamente", {filas_afectadas: filas})
+        });
     }
 }
 
